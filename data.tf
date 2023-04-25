81cad8d6385463d10507d708bb730a504298447c
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-25 11:19:37 +0530

    rabbitmq

--- data.tf
+++ data.tf
@@ -2,3 +2,11 @@ data "aws_ami" "centos-8-ami" {
   name_regex  = "surendra centos8"
   owners      = ["762942456166"]
 }
+
+data "aws_secretsmanager_secret" "roboshop" {
+  name = "roboshop/all-secrets"
+}
+
+data "aws_secretsmanager_secret_version" "roboshop" {
+  secret_id = data.aws_secretsmanager_secret.roboshop.id
+}
--- main.tf
+++ main.tf
@@ -2,9 +2,25 @@ resource "aws_instance" "rabbitmq" {
   ami                    = data.aws_ami.centos-8-ami.image_id
   instance_type          = var.instance_type
   subnet_id              = var.subnets[0]
-#  vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids = [aws_security_group.sg.id]
 #  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = "${var.env}-${var.name}-rabbitmq"
+  }
+}
+
+resource "null_resource" "ansible-apply" {
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_instance.rabbitmq.private_ip
+      user     = local.SSH_USER
+      password = local.SSH_PASS
+    }
+
+    inline = [
+      "sudo yum install python39-devel -y",
+      "sudo pip3.9 install ansible botocore boto3",
+      "ansible-pull -i localhost, -U https://github.com/surendrareddyalamuru/roboshop-ansible roboshop.yml -e ROLE_NAME=rabbitmq -e ENV=${var.env}"
+    ]
   }
 }
