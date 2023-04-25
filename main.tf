017f8f72e51e5c8d4120d7e7c0c34bf61187aee6
surendra reddy <surendrareddylamuru01@gmail.com>
2023-04-25 11:34:30 +0530

    rabbitmq

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "rabbitmq" {
   instance_type          = var.instance_type
   subnet_id              = var.subnets[0]
   vpc_security_group_ids = [aws_security_group.sg.id]
-#  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = "${var.env}-${var.name}-rabbitmq"
   }
